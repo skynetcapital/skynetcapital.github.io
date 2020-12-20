1b6074604d8ff9b79752d5d3b213251a9467c133
skynetcapital <67430414+skynetcapital@users.noreply.github.com>
2020-12-19 23:32:57 -0800

    feat: self-rebate frontend

---
+++ precache-manifest.e6fc2e1a1dc663d5463626a88c66805b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df81887551b26e9a51e235283e4bfee6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9de30512d108ce146e19",
+    "url": "/static/css/2.892654cc.chunk.css"
+  },
+  {
+    "revision": "d7abcc47d6e1ce241741",
+    "url": "/static/css/main.c206d5f3.chunk.css"
+  },
+  {
+    "revision": "9de30512d108ce146e19",
+    "url": "/static/js/2.e880241b.chunk.js"
+  },
+  {
+    "revision": "ed364e94a6f6b1bb953368935d752ffd",
+    "url": "/static/js/2.e880241b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7abcc47d6e1ce241741",
+    "url": "/static/js/main.6787564a.chunk.js"
+  },
+  {
+    "revision": "d3ebb56a3098223b5316",
+    "url": "/static/js/runtime-main.c7319a82.js"
+  },
+  {
+    "revision": "49174c736a9c871baaeb2db6d5372eee",
+    "url": "/static/media/logo.49174c73.svg"
+  }
+]);
