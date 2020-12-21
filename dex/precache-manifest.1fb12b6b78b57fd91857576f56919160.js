d96c35d68721273fad6635157d829cd6fcc4a4c4
skynetcapital <67430414+skynetcapital@users.noreply.github.com>
2020-12-20 18:10:45 -0800

    add swap

---
+++ dex/precache-manifest.1fb12b6b78b57fd91857576f56919160.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922beb5c7104145fe98cfd815503b99f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7b9794b50316c7f8cb59",
+    "url": "./static/css/2.b5a80ab8.chunk.css"
+  },
+  {
+    "revision": "108adaf6ffc2ed3439c9",
+    "url": "./static/css/main.ef7ff043.chunk.css"
+  },
+  {
+    "revision": "7b9794b50316c7f8cb59",
+    "url": "./static/js/2.3c0208d3.chunk.js"
+  },
+  {
+    "revision": "f5a8b6868b8c2bc8aba74532e51be46b",
+    "url": "./static/js/2.3c0208d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "108adaf6ffc2ed3439c9",
+    "url": "./static/js/main.b541ec8d.chunk.js"
+  },
+  {
+    "revision": "d852897cea0a694b3726",
+    "url": "./static/js/runtime-main.c06bb91b.js"
+  }
+]);
